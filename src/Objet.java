900c1b8f4066b15da279d4acbe3c6479033de94e
jeannot30 <heizmann.jean@hotmail.fr>
2015-04-27 15:28:34 +0200

    Update Objet.java
    Ajout de la gestion de l'image de l'objet + ajout de la vitesse dans les attributs d'objet

--- src/Objet.java
+++ src/Objet.java
@@ -1,12 +1,27 @@
+import java.awt.Image;
 import java.awt.geom.Point2D;
 import java.awt.geom.Point2D.Double;
 
-public abstract class  Objet {
-   private Point2D.Double position;  
-   private boolean actif;
+import java.io.File;
 
-   public Objet (double X, double Y, boolean actif){
-       position=new Point2D.Double(X,Y);
-       this.actif=actif; 
-   }
+import javax.imageio.ImageIO;
+
+public abstract class Objet {
+    private Point2D.Double position;
+    private boolean actif;
+    public Image image;
+    public float vitesse;
+
+    public Objet(double X, double Y, boolean actif, float vitesse, String NomImage) {
+        position = new Point2D.Double(X, Y);
+        this.vitesse = vitesse;
+        this.actif = actif;
+        // Récupérer l'image de l'objet 
+        try {
+            image = ImageIO.read(new File(NomImage));
+        } catch (Exception err) {
+            System.out.println("Image introuvable !");
+        }
+    }
 }
+
