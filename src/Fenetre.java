b91a429a6c47203739964837cc73da74db4b4141
Mikayah <olivier.bouillanne@gmail.com>
2015-04-30 14:22:03 +0200

    Update Fenetre.java
    Modification setResizable = true pour pouvoir tester =/= résolutions

--- src/Fenetre.java
+++ src/Fenetre.java
@@ -15,7 +15,7 @@ public class Fenetre extends JFrame {
         //récupération de la dimension réelle de l'écran et paramétrage des dimensions
         Dimension dimEcran = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
         setSize((int)dimEcran.getWidth(),(int)dimEcran.getHeight());
-        setResizable(false);
+        setResizable(true);
         
         //création d'une ouleur et paramétrage de la couleur de fond
         Color sky = new Color(27, 94, 161);
