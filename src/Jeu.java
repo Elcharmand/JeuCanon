d9c473af8c8895b0fa775fa13e8558a41b45158b
Elcharmand <armand.chefdor@gmail.com>
2015-05-04 15:35:39 +0200

    modification de Jeu en JFrame

--- src/Jeu.java
+++ src/Jeu.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 
 import javax.swing.*;
@@ -6,44 +7,116 @@ import javax.swing.*;
 public class Jeu extends JFrame {
 	
 	/**Attributs**/
-	
-    private Timer timer; 
+	private Timer timer; 
     private long temps;
     private Canon canon;
     private Boulet boulet;
-    
     private BufferedImage ArrierePlan;
-    public Graphics buffer;
-    public boolean ToucheHaut, ToucheBas, ToucheGauche, ToucheDroit, ToucheEspace;
-    public Rectangle Ecran;
+    private Graphics buffer;
+    private boolean ToucheHaut, ToucheBas, ToucheEspace;
+    private Rectangle EcranPanneau;
     private boolean finJeu;
     
     
+    
+    /**Constructeur**/
     public Jeu(){
-        // Gestion du temps 
+    	// Définition des dimensions du Frame
+        setSize(700,480);
+        setResizable(false);
+
         temps=0;
-        timer = new Timer(50,new TimerAction()); 
+        ToucheHaut=false;
+        ToucheBas=false;
+        ToucheEspace=false;
+        EcranPanneau=new Rectangle(getInsets().left,getInsets().top,getSize().width-getInsets().right-getInsets().left,getSize().height-getInsets().bottom-getInsets().top);
+        ArrierePlan =new BufferedImage(getSize().width,getSize().height,BufferedImage.TYPE_INT_RGB);
+        buffer = ArrierePlan.getGraphics();
+        
+        // déclaration du timer et des objets
+        timer = new Timer(60, new TimerAction());
+        
+        finJeu=false;
+        
         timer.start();
-        timer.setDelay(50);
-        //
-        // Gestion de la fenÃªtre 
-        fenetreJeu = new Fenetre("Plateau de jeu 1"); 
-        //
-        // CrÃ©ation des objets prÃ©sents 
-        // On crÃ©e le boulet : X, Y, actif , masse en kg, rayon en pixel 
-        boulet = new Boulet(0,0,true, 10, 10);      
-        // On crÃ©e le canon : X Y, actif, vitesse, angle
-        canon = new Canon (0,0, 10, 45);
-        finJeu= false; 
+        
+        this.addKeyListener(new Jeu_this_keyAdapter());
+        
+        //opérations de la fenêtre
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
     }
     
-    private class TimerAction implements ActionListener{
-        public void actionPerformed(ActionEvent e){
+    
+    
+  	/**Classe de gestion des listeners**/
+    private class Jeu_this_keyAdapter extends KeyAdapter {
+        
+        public void keyPressed (KeyEvent e) {
+            int code = e.getKeyCode();
+            switch (code) {
+            case KeyEvent.VK_SPACE :
+                ToucheEspace = true;
+                break;
+            case KeyEvent.VK_UP :
+                ToucheHaut = true;
+                break;
+            case KeyEvent.VK_DOWN :
+                ToucheBas = true;
+                break;
+            case KeyEvent.VK_ESCAPE :
+                System.exit(0);
+                break;
+            case KeyEvent.VK_ENTER :
+                if (timer.isRunning())
+                    timer.stop();
+                else
+                    timer.start();
+                break;
+            }
+
+        }
+        
+        
+        
+        public void keyReleased (KeyEvent e) {
+            int code = e.getKeyCode();
+            switch (code) {
+            case KeyEvent.VK_SPACE :
+                ToucheEspace = false;
+                break;
+            case KeyEvent.VK_UP :
+                ToucheHaut = false;
+                break;
+            case KeyEvent.VK_DOWN :
+                ToucheBas = false;
+                break;
+            }
+        }
+        
+    }
+    
+    
+    
+    /**Classe de gestion du timer**/
+    private class TimerAction implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            boucle_principale_jeu();
             temps++;
         }
     }
     
-    /*public static void main (String[] args) {
-        
-    }*/
+    
+    
+    /**méthode de gestion du plateau de jeu**/
+    public void boucle_principale_jeu() {
+    	
+    }
+    
+    
+    
+    /**Main**/
+    public static void main (String[] args) {
+        Jeu jeu1 = new Jeu ();
+    }
 }
