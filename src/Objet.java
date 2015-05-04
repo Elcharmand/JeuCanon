fa2e79b97d9bf1fd4cdcdbaa120210c170723364
jeannot30 <heizmann.jean@hotmail.fr>
2015-05-04 14:22:47 +0200

    Update Objet.java
    Changement de la postition en la définissant grâce à des coordonnées (X et Y) au lieu d'un Point2D

--- src/Objet.java
+++ src/Objet.java
@@ -7,13 +7,15 @@ import java.io.File;
 import javax.imageio.ImageIO;
 
 public abstract class Objet {
-    private Point2D.Double position;
+    private double X; 
+    private double Y; 
     private boolean actif;
     public Image image;
     public float vitesse;
-
+// Constructeur 
     public Objet(double X, double Y, boolean actif, float vitesse, String NomImage) {
-        position = new Point2D.Double(X, Y);
+        this.X=X; 
+        this.Y=Y;
         this.vitesse = vitesse;
         this.actif = actif;
         // Récupérer l'image de l'objet 
